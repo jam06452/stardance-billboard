df047929781506196f6da645384927e3b1da8ef8
q <jam@jam06452.uk>
2026-07-14 10:31:57 +0100

    Update auth.ex

--- lib/xinfeng_web/controllers/auth.ex
+++ lib/xinfeng_web/controllers/auth.ex
@@ -1,4 +1,4 @@
-defmodule Xinfeng.AuthController do
+defmodule XinfengWeb.AuthController do
   use XinfengWeb, :controller
 
   def on_success(conn, auth) do
@@ -9,17 +9,31 @@ defmodule Xinfeng.AuthController do
       slack_id: auth.slack_id
     }
 
-    case %Xinfeng.Accounts.User{}
-         |> Xinfeng.Accounts.User.changeset(user_attrs)
-         |> Xinfeng.Repo.insert() do
-      {:ok, _user} ->
+    auth_result =
+      case Xinfeng.Repo.get(Xinfeng.Accounts.User, auth.uid) do
+        nil ->
+          # New user: insert
+          %Xinfeng.Accounts.User{}
+          |> Xinfeng.Accounts.User.changeset(user_attrs)
+          |> Xinfeng.Repo.insert()
+
+        user ->
+          user
+          |> Xinfeng.Accounts.User.changeset(user_attrs)
+          |> Xinfeng.Repo.update()
+      end
+
+    case auth_result do
+      {:ok, user} ->
         conn
+        |> configure_session(renew: true)
+        |> put_session(:user_id, user.id)
         |> put_flash(:info, "Successfully authenticated")
-        |> redirect(to: ~p"/")
+        |> redirect(to: ~p"/dash")
 
       {:error, _changeset} ->
         conn
-        |> put_flash(:error, "Failed to create user")
+        |> put_flash(:error, "Failed to authenticate user")
         |> redirect(to: ~p"/")
     end
   end
@@ -27,6 +41,12 @@ defmodule Xinfeng.AuthController do
   def on_failure(conn, _reason) do
     conn
     |> put_flash(:error, "Authentication failed")
+    |> redirect(to: ~p"/")
+  end
+
+  def signout(conn, _params) do
+    conn
+    |> configure_session(drop: true)
     |> redirect(to: ~p"/")
   end
 end
