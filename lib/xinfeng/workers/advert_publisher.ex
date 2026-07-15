ce33c41cf8e5136e93d5cea7d6a292062fb1a8c0
q <jam@jam06452.uk>
2026-07-15 16:51:12 +0100

    Fixed image

--- lib/xinfeng/workers/advert_publisher.ex
+++ lib/xinfeng/workers/advert_publisher.ex
@@ -21,7 +21,7 @@ defmodule Xinfeng.Workers.AdvertPublisher do
       Xinfeng.Utils.edit_devlog(
         "Do you want to know how computers *actually* work? Do you want to remove the wool placed over your eyes by modern languages? Abstraction breeds ambiguity! Crawssembly is the first step to seeing clearly. The only assembly langiage made for education. (Now as fast as native C!)",
         [
-          "https://cdn.hackclub.com/019f609d-d621-7c78-86ac-d9e21dec7b33/9f7adca7-f169-47ad-8f9c-3519032103a7.png"
+          "https://cdn.hackclub.com/019f664b-7d26-728f-9e00-7c17cef7b930/c1081293-2a6d-4d5d-86ee-fdb58d9b7e0e.png"
         ],
         "https://stardance.hackclub.com/projects/8100"
       )
